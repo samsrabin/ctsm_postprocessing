a588b2fc178bfea6ebebf22365815e5452b93b4c
Sam Rabin <sam.rabin@gmail.com>
2026-01-21 13:12:10 -0700

    Crop: Delete unused cft_names and where.

--- crops/crop.py
+++ crops/crop.py
@@ -17,9 +17,7 @@ class Crop:
     Attributes:
         name (str): Name of the crop.
         cft_list (list): List of CFTs included in this crop.
-        cft_names (list): List of names of the CFTs.
         pft_nums (list): List of PFT numbers corresponding to the CFTs.
-        where (numpy.ndarray): Indices on the pft dimension corresponding to this crop.
     """
 
     def __init__(self, name, cft_list, ds):
@@ -41,14 +39,9 @@ class Crop:
             self.cft_list.append(cft)
 
         # Get information for all CFTs in this crop
-        self.cft_names = []
         self.pft_nums = []
-        self.where = np.array([], dtype=np.int64)
         for cft in self.cft_list:
-            self.cft_names.append(cft.name)
             self.pft_nums.append(cft.pft_num)
-            self.where = np.append(self.where, cft.get_where(ds))
-        self.where = np.sort(self.where)
 
     def __eq__(self, other):
         # Check that they're both Crops
