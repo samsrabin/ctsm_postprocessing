672b4c140e127adf631d2fbbaab55d25dc77a4b7
Sam Rabin <sam.rabin@gmail.com>
2026-01-21 12:55:30 -0700

    Crop: Delete unused 'pft_inds'.

--- crops/crop.py
+++ crops/crop.py
@@ -19,7 +19,6 @@ class Crop:
         cft_list (list): List of CFTs included in this crop.
         cft_names (list): List of names of the CFTs.
         pft_nums (list): List of PFT numbers corresponding to the CFTs.
-        pft_inds (list): List of PFT indices corresponding to the CFTs.
         where (numpy.ndarray): Indices on the pft dimension corresponding to this crop.
     """
 
@@ -44,12 +43,10 @@ class Crop:
         # Get information for all CFTs in this crop
         self.cft_names = []
         self.pft_nums = []
-        self.pft_inds = []
         self.where = np.array([], dtype=np.int64)
         for cft in self.cft_list:
             self.cft_names.append(cft.name)
             self.pft_nums.append(cft.pft_num)
-            self.pft_inds.append(cft.pft_ind)
             self.where = np.append(self.where, cft.get_where(ds))
         self.where = np.sort(self.where)
 
